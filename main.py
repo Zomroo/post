fd2c00dd63116d5de0e3b07988655eca0fce79ce
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-15 21:40:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# config.py
+
+api_id = '14091414'
+api_hash = '1e26ebacf23466ed6144d29496aa5d5b'
+bot_token = '5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo'
+authorized_users = [5500572462]  # List of authorized user IDs
+channel_id = -1001424450330  # Channel ID where the confirmed posts will be sent
