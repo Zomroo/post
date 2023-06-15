a83fd9e9ea20dae1d0b4893a459522d5dd361731
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 01:24:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ bot_token = '5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+
 # Handler for incoming messages
 @app.on_message(filters.private)
 def handle_message(client, message):
@@ -14,9 +15,9 @@ def handle_message(client, message):
     if message.text.startswith('http'):
         link = message.text
         
-        # Forward the link to the target channel
+        # Copy the message to the target channel
         channel_id = -1001424450330
-        client.forward_messages(chat_id=channel_id, from_chat_id=message.chat.id, message_ids=message.id, disable_notification=True)
+        client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.message_id)
 
 
 # Start the bot
