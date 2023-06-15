38d81e3b7581f2a9160812359711491d224c9655
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-15 22:31:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,36 @@
-# config.py
+import telebot
+import pymongo
 
-api_id = '14091414'
-api_hash = '1e26ebacf23466ed6144d29496aa5d5b'
-bot_token = '5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo'
-authorized_users = [5500572462]  # List of authorized user IDs
-channel_id = -1001424450330  # Channel ID where the confirmed posts will be sent
+bot = telebot.TeleBot("5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo")
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority")
+db = client["your_database"]
+collection = db["links"]
+
+# Define a function to save a link
+def save_link(link):
+    collection.insert_one({"link": link})
+
+# Define a function to send links to a channel
+def send_links(channel_id):
+    for link in collection.find():
+        bot.send_message(channel_id, link["link"])
+
+# Define a function to check if the bot is a member of a channel and an admin
+def is_member_and_admin(channel_id):
+    user = bot.get_user(channel_id)
+    return user.is_member and user.is_admin
+
+@bot.on_message
+def handle_message(message):
+    # Check if the message is a link
+    if message.text.startswith("https://"):
+        # Save the link
+        save_link(message.text)
+        # Check if the bot is a member of the channel and an admin
+        if is_member_and_admin(message.chat.id):
+            # Send the links to the channel
+            send_links(message.chat.id)
+
+bot.run()
