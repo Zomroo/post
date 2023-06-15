18702d7fe999292052a05935ef9a7ceb9b406d66
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-15 22:08:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,145 @@
+import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 
-# MongoDB connection details
-MONGODB_URI = "mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority"
-DATABASE_NAME = "Links"
-
-# Initialize MongoDB client
-mongo_client = MongoClient(MONGODB_URI)
-db = mongo_client[DATABASE_NAME]
+# MongoDB connection
+mongo_client = MongoClient("mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority")
+db = mongo_client["Loda"]
+links_collection = db["links"]
+images_collection = db["images"]
 
 # Telegram bot token
-BOT_TOKEN = "5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo"
+api_id = os.environ.get("14091414")
+api_hash = os.environ.get("1e26ebacf23466ed6144d29496aa5d5b")
+bot_token = os.environ.get("5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo")
 
-# Telegram channel ID
-CHANNEL_ID = -1001424450330
-
-# Telegram API details
-API_ID = "14091414"
-API_HASH = "1e26ebacf23466ed6144d29496aa5d5b"
-
-# Initialize Pyrogram client
-app = Client("my_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+# Create the Pyrogram client
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 
-@app.on_message(filters.private)
+def save_links(user_id, message_id, links):
+    # Save the links to the MongoDB collection
+    links_collection.insert_one({
+        "user_id": user_id,
+        "message_id": message_id,
+        "links": links
+    })
+
+
+def save_image(user_id, message_id, links, file_id):
+    # Save the image and links to the MongoDB collection
+    images_collection.insert_one({
+        "user_id": user_id,
+        "message_id": message_id,
+        "links": links,
+        "file_id": file_id
+    })
+
+
+@app.on_message(filters.private & (filters.text | filters.photo))
 def handle_private_message(client, message):
-    if message.from_user.is_bot:
-        return
+    user_id = message.from_user.id
+    chat_id = message.chat.id
 
-    # Check if the user is authorized
-    authorized_users = ["5500572462"]  # Add authorized usernames here
-    if message.from_user.username not in authorized_users:
-        return
+    if message.text:
+        # Extract links from the message text
+        links = message.text.split("\n")
 
-    # Extract links from message
-    links = []
-    if message.caption:
-        links = extract_links(message.caption)
+        if len(links) > 3:
+            client.send_message(chat_id, "Max link count exceeded. Please send up to 3 links.")
+            return
 
-    if not links and not message.photo:
-        return
+        if len(links) > 0:
+            # Save the links and send confirmation message
+            save_links(user_id, message.message_id, links)
+            confirm_text = "Links:\n\n" + "\n".join(links)
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Confirm", callback_data="confirm"),
+                 InlineKeyboardButton("Cancel", callback_data="cancel")]
+            ])
+            client.send_message(chat_id, confirm_text, reply_markup=keyboard)
+        else:
+            client.send_message(chat_id, "No links found in the message.")
 
-    # Prepare confirmation message
-    confirmation_text = "Please confirm the following links:\n\n"
-    for i, link in enumerate(links, start=1):
-        confirmation_text += f"Link {i}: {link}\n"
+    elif message.photo:
+        if message.caption:
+            # Extract links from the message caption
+            links = message.caption.split("\n")
 
-    if message.photo:
-        confirmation_text += "\n\nAttached Image"
+            if len(links) > 3:
+                client.send_message(chat_id, "Max link count exceeded. Please send up to 3 links.")
+                return
 
-    # Send confirmation message with confirm and cancel buttons
-    keyboard = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Confirm", callback_data="confirm"),
-                InlineKeyboardButton("Cancel", callback_data="cancel"),
-            ]
-        ]
-    )
-    client.send_message(message.chat.id, confirmation_text, reply_markup=keyboard)
+            if len(links) > 0:
+                # Save the image, links, and send confirmation message
+                file_id = message.photo.file_id
+                save_image(user_id, message.message_id, links, file_id)
+                confirm_text = "Image with Links:\n\n" + "\n".join(links)
+                keyboard = InlineKeyboardMarkup([
+                    [InlineKeyboardButton("Confirm", callback_data="confirm"),
+                     InlineKeyboardButton("Cancel", callback_data="cancel")]
+                ])
+                client.send_message(chat_id, confirm_text, reply_markup=keyboard)
+            else:
+                client.send_message(chat_id, "No links found in the message caption.")
+
+        else:
+            client.send_message(chat_id, "No links found in the message caption.")
 
 
 @app.on_callback_query()
 def handle_callback_query(client, callback_query):
-    if callback_query.from_user.is_bot:
-        return
+    user_id = callback_query.from_user.id
+    message_id = callback_query.message.message_id
+    data = callback_query.data
 
-    if callback_query.data == "confirm":
-        # Save links and image (if available) in the database
-        user_id = callback_query.from_user.id
-        links = extract_links(callback_query.message.text)
-        image_id = None
+    if data == "confirm":
+        # Retrieve the links from the MongoDB collection
+        if links_collection.count_documents({"user_id": user_id, "message_id": message_id}) > 0:
+            links_data = links_collection.find_one({"user_id": user_id, "message_id": message_id})
+            links = links_data["links"]
 
-        if callback_query.message.photo:
-            image_id = callback_query.message.photo[-1].file_id
+            # Process the links and send the post to the channel
+            # Modify this part according to your needs
+            post_text = "Links:\n\n"
+            buttons = []
 
-        save_links_in_database(user_id, links, image_id)
+            for i, link in enumerate(links, start=1):
+                post_text += f"Link{i}: [{link}]({link})\n"
+                buttons.append(InlineKeyboardButton(f"Link{i}", url=link))
 
-        # Send the post in the channel
-        send_post_in_channel(links, image_id)
+            reply_markup = InlineKeyboardMarkup([buttons])
+            app.send_message(-1001424450330, post_text, reply_markup=reply_markup)
 
-        # Send confirmation message to the user
-        client.send_message(
-            callback_query.message.chat.id, "Post sent successfully!"
-        )
+            # Clean up and delete the stored links
+            links_collection.delete_one({"user_id": user_id, "message_id": message_id})
 
-    elif callback_query.data == "cancel":
-        # Abort the process
-        client.send_message(callback_query.message.chat.id, "Process aborted!")
+        elif images_collection.count_documents({"user_id": user_id, "message_id": message_id}) > 0:
+            # Retrieve the image and links from the MongoDB collection
+            image_data = images_collection.find_one({"user_id": user_id, "message_id": message_id})
+            file_id = image_data["file_id"]
+            links = image_data["links"]
 
+            # Process the links and send the image and post to the channel
+            # Modify this part according to your needs
+            post_text = "Image with Links:\n\n"
+            buttons = []
 
-def extract_links(text):
-    # Extract links from the text
-    # You can use a regular expression or any other method here
-    # For simplicity, let's assume we are extracting URLs starting with "http://" or "https://"
-    links = []
-    words = text.split()
-    for word in words:
-        if word.startswith("http://") or word.startswith("https://"):
-            links.append(word)
-    return links
+            for i, link in enumerate(links, start=1):
+                post_text += f"Link{i}: [{link}]({link})\n"
+                buttons.append(InlineKeyboardButton(f"Link{i}", url=link))
 
+            reply_markup = InlineKeyboardMarkup([buttons])
+            app.send_photo(-1001424450330, file_id, caption=post_text, reply_markup=reply_markup)
 
-def save_links_in_database(user_id, links, image_id):
-    # Save links and image ID in the MongoDB collection
-    collection = db["links"]
-    data = {"user_id": user_id, "links": links, "image_id": image_id}
-    collection.insert_one(data)
+            # Clean up and delete the stored image and links
+            images_collection.delete_one({"user_id": user_id, "message_id": message_id})
 
-
-def send_post_in_channel(links, image_id):
-    # Prepare the post text with embedded links as buttons
-    post_text = "Links:\n\n"
-    for i, link in enumerate(links, start=1):
-        post_text += f"[Link {i}]({link})\n"
-
-    # Prepare the post message with the image (if available)
-    post_params = {"chat_id": CHANNEL_ID, "text": post_text, "parse_mode": "Markdown"}
-    if image_id:
-        post_params["photo"] = image_id
-
-    # Send the post message in the channel
-    app.send_message(**post_params)
+    elif data == "cancel":
+        client.send_message(callback_query.message.chat.id, "Process aborted.")
+        client.delete_messages(callback_query.message.chat.id, message_ids=message_id)
 
 
+# Start the bot
 app.run()
