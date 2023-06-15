a0db90813808e3410c86c5e22159906c3533e4f5
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 02:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,26 +21,26 @@ def handle_message(client, message):
 def handle_link_message(client, message):
     # Check if the message contains a link
     link = message.text
-
+        
     # Create the button with the link URL
     button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
     button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
     keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
-
+    
     # Send the confirmation message to the user
     confirm_msg = f"Are you sure you want to send this link?\n\n{link}"
-    client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard)
+    client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard, reply_to_message_id=message.id)
 
 
 def handle_photo_message(client, message):
     # Check if the message contains a link in the caption
     if message.caption and message.caption.startswith('http'):
         link = message.caption
-
+        
         # Create the button with the link URL
         button = InlineKeyboardButton(text="Click here", url=link)
         keyboard = InlineKeyboardMarkup([[button]])
-
+        
         # Send the photo with the link to the target channel
         channel_id = -1001424450330
         client.send_photo(chat_id=channel_id, photo=message.photo.file_id, caption=link, reply_markup=keyboard)
@@ -55,19 +55,19 @@ def handle_button_click(client, callback_query):
         message = callback_query.message
         if message.text and message.text.startswith('http'):
             link = message.text.split("\n\n")[1]
-
+            
             # Create the button with the link URL
             button = InlineKeyboardButton(text="Click here", url=link)
             keyboard = InlineKeyboardMarkup([[button]])
-
+            
             # Send the message to the target channel
             channel_id = -1001424450330
             caption = f"Link: {link}"
             client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
-
+            
             # Delete the original message
-            client.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
-
+            client.delete_messages(chat_id=message.chat.id, message_ids=message.id)
+    
     elif callback_query.data == "cancel":
         # Delete the confirmation message
         client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.message_id)
