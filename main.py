d3fa2c8cb81a87d6f2ea395b6659c3d57335050c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 02:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,45 +13,31 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 @app.on_message(filters.private)
 def handle_message(client, message):
     # Check if the message contains a link
-    if message.text.startswith('http') or message.photo:
+    if message.text.startswith('http'):
         link = message.text
-        photo = message.photo.file_id if message.photo else None
         
-        # Create the button with the link URL
-        button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
-        button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
-        keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
-        
-        # Send the confirmation message to the user
-        confirm_msg = f"Are you sure you want to send this link?\n\n{link}"
-        client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard, photo=photo)
-
-
-# Handler for button callbacks
-@app.on_callback_query()
-def handle_button_click(client, callback_query):
-    # Check the callback data
-    if callback_query.data == "confirm":
-        # Get the original message data
-        message = callback_query.message
-        link = message.text.split("\n\n")[1]
-        photo = message.photo.file_id if message.photo else None
-        
-        # Create the button with the link URL
-        button = InlineKeyboardButton(text="Click here", url=link)
-        keyboard = InlineKeyboardMarkup([[button]])
-        
-        # Send the message to the target channel
-        channel_id = -1001424450330
-        caption = f"Link: {link}"
-        client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard, photo=photo)
-        
-        # Delete the original message
-        client.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
-    
-    elif callback_query.data == "cancel":
-        # Delete the confirmation message
-        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.message_id)
+        # Check if the message has an attached photo
+        if message.photo:
+            # Get the photo file ID and send it with the link as a caption
+            photo = message.photo[-1]
+            
+            # Create the button with the link URL
+            button = InlineKeyboardButton(text="Click here", url=link)
+            keyboard = InlineKeyboardMarkup([[button]])
+            
+            # Send the photo with the link as a caption to the target channel
+            channel_id = -1001424450330
+            caption = f"Link: {link}"
+            client.send_photo(chat_id=channel_id, photo=photo.file_id, caption=caption, reply_markup=keyboard)
+        else:
+            # Create the button with the link URL
+            button = InlineKeyboardButton(text="Click here", url=link)
+            keyboard = InlineKeyboardMarkup([[button]])
+            
+            # Send the message with the link as a caption to the target channel
+            channel_id = -1001424450330
+            caption = f"Link: {link}"
+            client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
 
 
 # Start the bot
