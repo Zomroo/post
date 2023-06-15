1f8cec9df10d5d4a1b53b1564bbb4d7bd055038c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 01:14:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ bot_token = '5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Replace '-1001424450330' with your own channel ID
-channel_id = -1001424450330
+# Handler for incoming messages
+@app.on_message(filters.private)
+def handle_message(client, message):
+    # Check if the message contains a link
+    if message.text.startswith('http'):
+        link = message.text
+        
+        # Forward the link to the target channel
+        channel_id = -1001424450330
+        client.forward_messages(chat_id=channel_id, from_chat_id=message.chat.id, message_ids=message.message_id)
 
-# Define the filter to only handle messages with links
-@filters.regex(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-def handle_links(client, message, update):
-    link = message.text
-    client.send_message(chat_id=channel_id, text=link)
-
-# Register the filter and the corresponding handler function
-app.add_handler(handle_links)
 
 # Start the bot
 app.run()
