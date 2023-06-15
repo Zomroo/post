7613a8ce545e7a97c10f2e68fc1cddfd2cb095a9
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-15 23:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,51 +1,44 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from pyrogram import Client, Filters
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-# Define your bot token here
+# Define your API hash, API ID, and bot token here
+API_ID = 'YOUR_API_ID'
+API_HASH = 'YOUR_API_HASH'
 TOKEN = '5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo'
 
-# Create an Updater object and pass your bot token
-updater = Updater(TOKEN, use_context=True)
-
-# Get the dispatcher to register handlers
-dispatcher = updater.dispatcher
+# Create a Pyrogram client
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 # Define a command handler for the start command
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please send me a link and I'll forward it to all the channels and groups where I'm an admin.")
-
-# Register the start command handler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+@app.on_message(Filters.command("start"))
+def start(client, message):
+    client.send_message(chat_id=message.chat.id, text="I'm a bot, please send me a link and I'll forward it to all the channels and groups where I'm an admin.")
 
 # Define a message handler
-def forward_link(update, context):
+@app.on_message(~Filters.command)
+def forward_link(client, message):
     # Get the link from the message
-    link = update.message.text
+    link = message.text
     
     # Check if the bot is an admin in the chat
-    chat_member = context.bot.get_chat_member(update.effective_chat.id, context.bot.id)
+    chat_member = client.get_chat_member(chat_id=message.chat.id, user_id=client.get_me().id)
     if chat_member.status == "administrator":
         # Get the list of all chats where the bot is added
-        chats = context.bot.getChatAdministrators(update.effective_chat.id)
+        chats = client.get_chat_members_count(chat_id=message.chat.id).count
     
         # Forward or send the link to all chats
         for chat in chats:
             try:
-                context.bot.send_message(chat_id=chat.chat.id, text=link)
+                client.send_message(chat_id=chat.chat.id, text=link)
             except Exception as e:
                 # Handle any errors that occur during sending
                 print(f"Error forwarding link to chat {chat.chat.title}: {str(e)}")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I can't forward links to chats where I'm not an admin.")
-
-# Register the message handler
-link_handler = MessageHandler(Filters.text & (~Filters.command), forward_link)
-dispatcher.add_handler(link_handler)
+        client.send_message(chat_id=message.chat.id, text="Sorry, I can't forward links to chats where I'm not an admin.")
 
 # Start the bot
-updater.start_polling()
+app.run()
