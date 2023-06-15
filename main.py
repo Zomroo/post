670676cd34c784c047360da38f3c4e430100daac
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 02:06:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,46 +12,65 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 # Handler for incoming messages
 @app.on_message(filters.private)
 def handle_message(client, message):
-    if message.text:
-        # Check if the message contains a link
-        if message.text.startswith('http'):
-            link = message.text
-            
-            # Create the button with the link URL
-            button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
-            button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
-            keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
-            
-            # Send the confirmation message to the user
-            confirm_msg = f"Are you sure you want to send this link?\n\n{link}"
-            client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard)
-
+    if message.text.startswith('http'):
+        handle_link_message(client, message)
     elif message.photo:
-        # Get the photo from the message
-        photo = message.photo
+        handle_photo_message(client, message)
+
+
+def handle_link_message(client, message):
+    # Check if the message contains a link
+    link = message.text
         
-        # Check if the message has a caption containing a link
-        if photo.caption and photo.caption.startswith('http'):
-            link = photo.caption
+    # Create the button with the link URL
+    button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
+    button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
+    keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
+    
+    # Send the confirmation message to the user
+    confirm_msg = f"Are you sure you want to send this link?\n\n{link}"
+    client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard)
+
+
+def handle_photo_message(client, message):
+    # Check if the message contains a link in the caption
+    if message.caption and message.caption.startswith('http'):
+        link = message.caption
+        
+        # Create the button with the link URL
+        button = InlineKeyboardButton(text="Click here", url=link)
+        keyboard = InlineKeyboardMarkup([[button]])
+        
+        # Send the photo with the link to the target channel
+        channel_id = -1001424450330
+        client.send_photo(chat_id=channel_id, photo=message.photo.file_id, caption=link, reply_markup=keyboard)
+
+
+# Handler for button callbacks
+@app.on_callback_query()
+def handle_button_click(client, callback_query):
+    # Check the callback data
+    if callback_query.data == "confirm":
+        # Get the original message data
+        message = callback_query.message
+        if message.text.startswith('http'):
+            link = message.text.split("\n\n")[1]
             
             # Create the button with the link URL
-            button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
-            button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
-            keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
+            button = InlineKeyboardButton(text="Click here", url=link)
+            keyboard = InlineKeyboardMarkup([[button]])
             
-            # Send the confirmation message to the user
-            confirm_msg = f"Are you sure you want to send this link?\n\n{link}"
-            client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard)
-            
-            # Send the photo along with the link
-            client.send_photo(chat_id=message.chat.id, photo=photo.file_id, caption=link, reply_markup=keyboard)
+            # Send the message to the target channel
+            channel_id = -1001424450330
+            caption = f"Link: {link}"
+            client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
             
             # Delete the original message
             client.delete_messages(chat_id=message.chat.id, message_ids=message.id)
     
     elif callback_query.data == "cancel":
         # Delete the confirmation message
-        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.message_id)
+        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.id)
 
 
 # Start the bot
