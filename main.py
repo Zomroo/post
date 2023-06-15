4cc9d20789ea05472266ffa557303b68bbf70c66
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 01:32:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,34 +17,40 @@ def handle_message(client, message):
         link = message.text
         
         # Create the button with the link URL
-        button_confirm = InlineKeyboardButton(text="Confirm", callback_data="confirm")
+        button_confirm = InlineKeyboardButton(text="Confirm", callback_data=f"confirm_{message.message_id}")
         button_cancel = InlineKeyboardButton(text="Cancel", callback_data="cancel")
         keyboard = InlineKeyboardMarkup([[button_confirm, button_cancel]])
         
-        # Send the confirmation message
+        # Send the confirmation message as a reply to the user's original message
         confirm_msg = f"Do you want to send this link?\n\nLink: {link}"
-        client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard)
+        client.send_message(chat_id=message.chat.id, text=confirm_msg, reply_markup=keyboard, reply_to_message_id=message.message_id)
 
 
 # Handler for button callbacks
 @app.on_callback_query()
 def handle_callback(client, callback_query):
-    if callback_query.data == "confirm":
-        # Retrieve the link from the callback message
-        link = callback_query.message.reply_to_message.text[6:]  # Remove "Link: " from the beginning
+    if callback_query.data.startswith("confirm_"):
+        message_id = int(callback_query.data.split("_")[1])
+        original_message = client.get_messages(callback_query.message.chat.id, message_ids=message_id)
         
-        # Create the button with the link URL
-        button = InlineKeyboardButton(text="Click here", url=link)
-        keyboard = InlineKeyboardMarkup([[button]])
+        if original_message.reply_to_message and original_message.reply_to_message.text.startswith("Link:"):
+            link = original_message.reply_to_message.text[6:]  # Remove "Link: " from the beginning
+            
+            # Create the button with the link URL
+            button = InlineKeyboardButton(text="Click here", url=link)
+            keyboard = InlineKeyboardMarkup([[button]])
+            
+            # Send the message to the target channel
+            channel_id = -1001424450330
+            caption = f"Link: {link}"
+            client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
         
-        # Send the message to the target channel
-        channel_id = -1001424450330
-        caption = f"Link: {link}"
-        client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
-        
+        # Delete the confirmation message
+        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.message_id)
+    
     elif callback_query.data == "cancel":
-        # No need to do anything if the user clicks "Cancel"
-        pass
+        # Delete the confirmation message
+        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.message_id)
 
 
 # Start the bot
