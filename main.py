003826279d55d89d7699b4b1cc75e67d89cdcd22
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-23 14:44:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,11 +75,11 @@ def handle_callback(client, callback_query):
         if message.photo:
             # Copy the image and link to the target channel
             channel_id = -1001424450330
-            caption = f"Title - {message.caption.split('\n')[0]}\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl")
+            caption = "Title - {}\n\nJoin Backup Channel - {}".format(message.caption.split('\n')[0], "https://t.me/+jUtnpvdlE9AwZTRl")
             caption_links = message.caption.split('\n')[1:]
             buttons = []
             for i in range(min(3, len(caption_links))):
-                buttons.append(InlineKeyboardButton(text=f"Link {i + 1}", url=caption_links[i]))
+                buttons.append(InlineKeyboardButton(text="Link {}".format(i + 1), url=caption_links[i]))
             keyboard = InlineKeyboardMarkup([buttons])
             client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.id,
                                 caption=caption, reply_markup=keyboard)
@@ -88,8 +88,8 @@ def handle_callback(client, callback_query):
             channel_id = -1001424450330
             links = message.text if message.text.startswith('http') else message.caption
             links = links.split('\n')[1:4]  # Limit to a maximum of 3 links
-            caption = f"Title - {message.caption.split('\n')[0]}\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl"
-            buttons = [InlineKeyboardButton(text=f"Link {i + 1}", url=link) for i, link in enumerate(links)]
+            caption = "Title - {}\n\nJoin Backup Channel - {}".format(message.caption.split('\n')[0], "https://t.me/+jUtnpvdlE9AwZTRl")
+            buttons = [InlineKeyboardButton(text="Link {}".format(i + 1), url=link) for i, link in enumerate(links)]
             keyboard = InlineKeyboardMarkup([buttons])
             client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
 
