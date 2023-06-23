bbeda1372430ba6e49550cbb3af5b6869b9e67cf
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-23 19:51:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,40 +74,32 @@ def handle_callback(client, callback_query):
         # Get the original message
         message = client.get_messages(chat_id=callback_query.message.chat.id, message_ids=message_id)
 
-        print("Original Message:")
-        print(message)
-
         if message.photo:
-            # Copy the image and link to the target channel
-            channel_id = -1001424450330
+        # Copy the image and link to the target channel
+        channel_id = -1001424450330
+        caption = f"Links:\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n{message.caption}"
+        caption_links = message.caption.split('\n')
+        buttons = []
+        for i in range(min(3, len(caption_links))):
+            buttons.append(InlineKeyboardButton(text=f"Link {i+1}", url=caption_links[i]))
+        keyboard = InlineKeyboardMarkup([buttons])
+        client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.id, caption=caption, reply_markup=keyboard)
+
+    else:
+        # Send the links as a message to the target channel
+        channel_id = -1001424450330
+        links = message.text if message.text and message.text.startswith('http') else message.caption
+        if links:
+            links = links.split('\n')[:3]  # Limit to a maximum of 3 links
             caption = f"Links:\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n{message.caption}"
-            caption_links = message.caption.split('\n')
-            buttons = []
-            for i in range(min(3, len(caption_links))):
-                buttons.append(InlineKeyboardButton(text=f"Link {i+1}", url=caption_links[i]))
+            buttons = [InlineKeyboardButton(text=f"Link {i+1}", url=link) for i, link in enumerate(links)]
             keyboard = InlineKeyboardMarkup([buttons])
-            client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.id, caption=caption, reply_markup=keyboard)
+            client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
 
-            print("Copied Message:")
-            print(message)
-        else:
-            # Send the links as a message to the target channel
-            channel_id = -1001424450330
-            links = message.text if message.text and message.text.startswith('http') else message.caption
-            if links:
-                links = links.split('\n')[:3]  # Limit to a maximum of 3 links
-                caption = f"Links:\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n{message.caption}"
-                buttons = [InlineKeyboardButton(text=f"Link {i+1}", url=link) for i, link in enumerate(links)]
-                keyboard = InlineKeyboardMarkup([buttons])
-                client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
 
-                print("Sent Message:")
-                print(message)
+    # Delete the confirmation message
+    client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.id)
 
-        # Delete the confirmation message
-        client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.id)
-
-        print("Confirmation Message Deleted")
     
     elif action == 'cancel':
         # Delete the confirmation message and the original message
