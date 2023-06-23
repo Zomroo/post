d3e846515926922730bfb81fc13a22912df9e0ce
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-23 19:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Create a Pyrogram client
 api_id = 14091414
