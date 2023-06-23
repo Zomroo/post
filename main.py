223968a54421ea73cf80b7b24b2cb55a70a6d35e
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-23 21:21:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,11 +71,11 @@ def handle_callback(client, callback_query):
     callback_data = callback_query.data.split('_')
     action = callback_data[0]
     message_id = int(callback_data[1])
-    
+
     if action == 'confirm':
         # Get the original message
         message = client.get_messages(chat_id=callback_query.message.chat.id, message_ids=message_id)
-        
+
         if message.photo:
             # Copy the image and link to the target channel
             channel_id = -1001424450330
@@ -83,7 +83,7 @@ def handle_callback(client, callback_query):
             caption_links = message.caption.split('\n')
             buttons = []
             for i in range(min(3, len(caption_links))):
-                buttons.append([InlineKeyboardButton(text=f"Link {i+1}", url=caption_links[i])])
+                buttons.append(InlineKeyboardButton(text=f"Link {i+1}", url=caption_links[i]))
             keyboard = InlineKeyboardMarkup(buttons)
             client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.id, caption=caption, reply_markup=keyboard)
         else:
@@ -92,13 +92,13 @@ def handle_callback(client, callback_query):
             links = message.text if message.text.startswith('http') else message.caption
             links = links.split('\n')[:3] if links else []  # Limit to a maximum of 3 links
             caption = f"{message.caption}\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl"
-            buttons = [[InlineKeyboardButton(text=f"Link {i+1}", url=link)] for i, link in enumerate(links)]
+            buttons = [InlineKeyboardButton(text=f"Link {i+1}", url=link) for i, link in enumerate(links)]
             keyboard = InlineKeyboardMarkup(buttons)
             client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
-        
+
         # Delete the confirmation message
         client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.id)
-    
+
     elif action == 'cancel':
         # Delete the confirmation message and the original message
         client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=[callback_query.message.id, message_id])
