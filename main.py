a7ba9e59840a48c9e051c975e1bbbe056ea37a60
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-23 23:52:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 # Create a Pyrogram client
 api_id = 14091414
 api_hash = '1e26ebacf23466ed6144d29496aa5d5b'
-bot_token = '6182295264:AAEEilY-tEUOn3KzxT7CRNnukkKTOYrZFnQ'
+bot_token = '5615528335:AAFrJcGIItkdEvMZREvOi3LgLKeNHu9Md2c'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
@@ -18,12 +18,18 @@ def is_authorized(user_id):
     return user_id in authorized_users
 
 
+# Variable to store text after links
+text_after_links = ''
+
+
 # Handler for incoming messages
 @app.on_message(filters.private)
 def handle_message(client, message):
     # Check if user is authorized
     if not is_authorized(message.from_user.id):
         return  # Ignore non-authorized users
+
+    global text_after_links  # Use the global variable
 
     if message.text:
         # Check if the message contains a link in text
@@ -37,6 +43,9 @@ def handle_message(client, message):
             keyboard = InlineKeyboardMarkup([[confirm_button, cancel_button]])
 
             client.send_message(chat_id=message.chat.id, text=confirmation_message, reply_markup=keyboard)
+        else:
+            # Store text after links
+            text_after_links = message.text.strip()  # Remove leading/trailing whitespace
     
     if message.caption:
         # Check if the message contains a link in caption
@@ -50,6 +59,9 @@ def handle_message(client, message):
             keyboard = InlineKeyboardMarkup([[confirm_button, cancel_button]])
 
             client.send_message(chat_id=message.chat.id, text=confirmation_message, reply_markup=keyboard)
+        else:
+            # Store text after links
+            text_after_links = message.caption.strip()  # Remove leading/trailing whitespace
     
     # Delete the message if it doesn't contain a link
     if not (message.text or message.caption):
@@ -67,6 +79,8 @@ def handle_callback(client, callback_query):
     action = callback_data[0]
     message_id = int(callback_data[1])
     
+    global text_after_links  # Use the global variable
+
     if action == 'confirm':
         # Get the original message
         message = client.get_messages(chat_id=callback_query.message.chat.id, message_ids=message_id)
@@ -74,30 +88,41 @@ def handle_callback(client, callback_query):
         if message.photo:
             # Copy the image and link to the target channel
             channel_id = -1001424450330
+            caption = f"**Title** : `Leaks` \n\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n\n**Click On The Button Below To Get The Videos**"
             caption_links = message.caption.split('\n')
-            links = caption_links[:3]  # Limit to a maximum of 3 links
-            text = '\n'.join(caption_links[3:])  # Extract the additional text
-            caption = f"**Title** : `Leaks` \n\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n\n**Click On The Button Below To Get The Videos**\n\n{text}"
             buttons = []
-            for i in range(min(3, len(links))):
-                buttons.append(InlineKeyboardButton(text=f"Link {i+1}", url=links[i]))
+            for i in range(min(3, len(caption_links))):
+                buttons.append(InlineKeyboardButton(text=f"Link {i+1}", url=caption_links[i]))
             keyboard = InlineKeyboardMarkup([buttons])
+            
+            if text_after_links:
+                caption += '\n\n' + text_after_links  # Include text after links in caption
+            
             client.copy_message(chat_id=channel_id, from_chat_id=message.chat.id, message_id=message.id, caption=caption, reply_markup=keyboard)
         else:
-            # Send the links and additional text as a message to the target channel
+            # Send the links as a message to the target channel
             channel_id = -1001424450330
             links = message.text if message.text.startswith('http') else message.caption
             links = links.split('\n')[:3]  # Limit to a maximum of 3 links
-            text = '\n'.join(links[3:])  # Extract the additional text
-            caption = f"**Title** : `Leaks` \n\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n**Click On The Button Below To Get The Videos**\n\n{text}"
+            caption = f"**Title** : `Leaks` \n\n\nJoin Backup Channel - https://t.me/+jUtnpvdlE9AwZTRl\n\n**Click On The Button Below To Get The Videos**"
             buttons = [InlineKeyboardButton(text=f"Link {i+1}", url=link) for i, link in enumerate(links)]
             keyboard = InlineKeyboardMarkup([buttons])
+            
+            if text_after_links:
+                caption += '\n\n' + text_after_links  # Include text after links in caption
+            
             client.send_message(chat_id=channel_id, text=caption, reply_markup=keyboard)
+        
+        # Reset text_after_links
+        text_after_links = ''
         
         # Delete the confirmation message
         client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.id)
     
     elif action == 'cancel':
+        # Reset text_after_links
+        text_after_links = ''
+        
         # Delete the confirmation message and the original message
         client.delete_messages(chat_id=callback_query.message.chat.id, message_ids=[callback_query.message.id, message_id])
 
